25688c8bbcc981dd6660536a71cf987524c354ce
Rakesh Bangaru <s4112249@student.rmit.edu.au>
2026-07-22 17:41:03 +1000

    Login Page created for Console Banking Application

---
+++ BankingApplication/Program.cs
@@ -0,0 +1,30 @@
+﻿class BankingApplication
+{
+    static void Main()
+    {
+        System.Console.WriteLine("Welcome to the Micro Bank");
+        System.Console.WriteLine("***** Login Page *****");
+
+        
+
+        System.Console.Write("Username: ");
+        string username = System.Console.ReadLine() ?? "";
+        string password = "";
+
+        if (!string.IsNullOrWhiteSpace(username))
+        {
+            System.Console.Write("Password: ");
+             password = System.Console.ReadLine() ?? "";
+        }
+
+        if (username == "admin" && password == "123")
+        {
+            System.Console.WriteLine("Welcome Admin...");
+            System.Console.WriteLine("***** Main Menu *****");
+        }
+        else
+        {
+            System.Console.WriteLine("Invalid username or password.");
+        }
+    }
+}
