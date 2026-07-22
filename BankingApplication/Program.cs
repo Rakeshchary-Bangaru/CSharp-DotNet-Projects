f14b64e2ba5c09a9b568b24a3c91678ae3c8535c
Rakesh Bangaru <s4112249@student.rmit.edu.au>
2026-07-22 18:13:04 +1000

    Created Main Menu

--- BankingApplication/Program.cs
+++ BankingApplication/Program.cs
@@ -1,4 +1,6 @@
-﻿class BankingApplication
+﻿using System.Diagnostics;
+
+class BankingApplication
 {
     static void Main()
     {
@@ -21,10 +23,49 @@
         {
             System.Console.WriteLine("Welcome Admin...");
             System.Console.WriteLine("***** Main Menu *****");
+
+            int mainmenuchoice = -1;
+
+
+            do
+            {
+                // Show Main Menu
+                System.Console.WriteLine("\n ::: Main Menu :::");
+                System.Console.WriteLine("1. Customers");
+                System.Console.WriteLine("2. Accounts");
+                System.Console.WriteLine("3. Transaction");
+                System.Console.WriteLine("4. Transfer Statements");
+                System.Console.WriteLine("5. Account Statement");
+                System.Console.WriteLine("0. Exit");
+
+                //Accept menu choice from user 
+                System.Console.WriteLine("Enter your choice: ");
+                mainmenuchoice = int.Parse(System.Console.ReadLine() ?? "0");
+
+                switch (mainmenuchoice)
+                {
+                    case 1 : System.Console.WriteLine("Customers");// Customers
+                        break;
+                    case 2 : System.Console.WriteLine("Accounts");// Accounts
+                        break;
+                    case 3 : System.Console.WriteLine("Transaction");// Transactions
+                        break;
+                    case 4 : System.Console.WriteLine("Transfer Statements");// Transfer Statements
+                        break;
+                    case 5 : System.Console.WriteLine("Account Statement");// Account Statement
+                        break;   
+                
+
+                }
+            } while (mainmenuchoice != 0);
+
+            
         }
         else
         {
             System.Console.WriteLine("Invalid username or password.");
         }
+
+        System.Console.WriteLine("Thank you visit again...");
     }
 }
